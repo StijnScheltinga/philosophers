7a048d15dbc1a8c15e6b4b82882b5c4078c23aa5
Stijn <stijnskoop@gmail.com>
2023-04-25 17:39:27 +0200

    removed forks for know, need to indicate threads first

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: sschelti <sschelti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 18:12:12 by sschelti          #+#    #+#             */
-/*   Updated: 2023/04/25 12:45:03 by sschelti         ###   ########.fr       */
+/*   Updated: 2023/04/25 17:09:30 by sschelti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,16 @@ typedef struct s_rules{
 	unsigned int	time_to_eat;
 	unsigned int	time_to_sleep;
 	unsigned int	number_of_times_each_philosopher_must_eat;
-	pthread_mutex_t	*forks;
-	pthread_t		*philo_id;
 }	t_rules;
+
+typedef struct s_philo_data{
+	int		which_philo;
+}	t_philo_data;
 
 int				set_rules(t_rules *rules, int argc, char **argv);
 int				check_values(int argc, char **argv);
 int				create_philosophers(t_rules *rules);
-void			*philosopher(void *rules);
-void			create_forks(t_rules *rules);
-void			destroy_forks(t_rules *rules);
+void			*philosopher(void *i);
 
 unsigned int	ft_uatoi(const char *str);
 int				ft_strlen(const char *str);
--- src/create_threads.c
+++ src/create_threads.c
@@ -6,70 +6,45 @@
 /*   By: sschelti <sschelti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 14:23:28 by sschelti          #+#    #+#             */
-/*   Updated: 2023/04/25 12:59:13 by sschelti         ###   ########.fr       */
+/*   Updated: 2023/04/25 17:13:56 by sschelti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	*philosopher(void *rules)
+void	*philosopher(void *philo_data)
 {
-	t_rules	*data;
+	t_philo_data	*philo;
 
-	data = (t_rules *)rules;
+	philo = (t_philo_data *)philo_data;
+	
+	printf("%d\n", j);
 	return (NULL);
 }
 
 int	create_philosophers(t_rules *rules)
 {
+	pthread_t		*philo_id;
+	t_philo_data	philo_data;
 	int				i;
 
-	rules->philo_id = malloc(rules->number_of_philosophers * sizeof(pthread_t));
-	if (!rules->philo_id)
+	philo_id = malloc(rules->number_of_philosophers * sizeof(pthread_t));
+	if (!philo_id)
 		return (1);
 	i = 0;
-	create_forks(rules);
 	while (i != rules->number_of_philosophers)
 	{
-		if (pthread_create(&rules->philo_id[i], NULL, &philosopher, rules) != 0)
+		philo_data.which_philo = i + 1;
+		if (pthread_create(&philo_id[i], NULL, &philosopher, &philo_data) != 0)
 			return (1);
 		i++;
 	}
 	i = 0;
 	while (i != rules->number_of_philosophers)
 	{
-		pthread_join(rules->philo_id[i], NULL);
+		pthread_join(philo_id[i], NULL);
 		i++;
 	}
-	destroy_forks(rules);
-	free (rules->philo_id);
+	free (philo_id);
 	return (0);
-}
-
-void	create_forks(t_rules *rules)
-{
-	int	number_of_forks;
-	int	i;
-
-	number_of_forks = rules->number_of_philosophers;
-	rules->forks = malloc(number_of_forks * sizeof(pthread_mutex_t));
-	while (i != number_of_forks)
-	{
-		pthread_mutex_init(&rules->forks[i], NULL);
-		i++;
-	}
-}
-
-void	destroy_forks(t_rules *rules)
-{
-	int	number_of_forks;
-	int	i;
-
-	number_of_forks = rules->number_of_philosophers;
-	while (i != number_of_forks)
-	{
-		pthread_mutex_destroy(&rules->forks[i]);
-		i++;
-	}
-	free(rules->forks);
 }

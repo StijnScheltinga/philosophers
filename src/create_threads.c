61b2513a706c604bee879cf048e740c487218ce0
Stijn <stijnskoop@gmail.com>
2023-04-25 13:00:04 +0200

    added philosopher and fork array + leaks check

--- src/create_threads.c
+++ src/create_threads.c
@@ -6,39 +6,70 @@
 /*   By: sschelti <sschelti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 14:23:28 by sschelti          #+#    #+#             */
-/*   Updated: 2023/04/24 18:18:51 by sschelti         ###   ########.fr       */
+/*   Updated: 2023/04/25 12:59:13 by sschelti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	*ft_test(void *in)
+void	*philosopher(void *rules)
 {
-	printf("jo\n");
+	t_rules	*data;
+
+	data = (t_rules *)rules;
 	return (NULL);
 }
 
 int	create_philosophers(t_rules *rules)
 {
-	pthread_t	*philo_id;
-	int			i;
+	int				i;
 
-	philo_id = (pthread_t *)malloc(rules->number_of_philosophers * sizeof(pthread_t));
-	if (!philo_id)
+	rules->philo_id = malloc(rules->number_of_philosophers * sizeof(pthread_t));
+	if (!rules->philo_id)
 		return (1);
 	i = 0;
+	create_forks(rules);
 	while (i != rules->number_of_philosophers)
 	{
-		if (pthread_create(&philo_id[i], NULL, &ft_test, NULL) != 0)
+		if (pthread_create(&rules->philo_id[i], NULL, &philosopher, rules) != 0)
 			return (1);
 		i++;
 	}
 	i = 0;
 	while (i != rules->number_of_philosophers)
 	{
-		pthread_join(philo_id[i], NULL);
+		pthread_join(rules->philo_id[i], NULL);
 		i++;
 	}
-	free (philo_id);
+	destroy_forks(rules);
+	free (rules->philo_id);
 	return (0);
+}
+
+void	create_forks(t_rules *rules)
+{
+	int	number_of_forks;
+	int	i;
+
+	number_of_forks = rules->number_of_philosophers;
+	rules->forks = malloc(number_of_forks * sizeof(pthread_mutex_t));
+	while (i != number_of_forks)
+	{
+		pthread_mutex_init(&rules->forks[i], NULL);
+		i++;
+	}
+}
+
+void	destroy_forks(t_rules *rules)
+{
+	int	number_of_forks;
+	int	i;
+
+	number_of_forks = rules->number_of_philosophers;
+	while (i != number_of_forks)
+	{
+		pthread_mutex_destroy(&rules->forks[i]);
+		i++;
+	}
+	free(rules->forks);
 }
--- src/main.c
+++ src/main.c
@@ -6,16 +6,22 @@
 /*   By: sschelti <sschelti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 18:01:05 by sschelti          #+#    #+#             */
-/*   Updated: 2023/04/24 14:24:53 by sschelti         ###   ########.fr       */
+/*   Updated: 2023/04/25 12:48:16 by sschelti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
+void	leaks(void)
+{
+	system("leaks philo");
+}
+
 int	main(int argc, char **argv)
 {
 	t_rules	rules;
 
+	atexit(leaks);
 	if (set_rules(&rules, argc, argv) == 1)
 		return (1);
 	print_struct(&rules);

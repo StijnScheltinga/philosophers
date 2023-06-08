7df169b2ae109406aebe4e0935f0ca8ab6581388
Stijn <stijnskoop@gmail.com>
2023-06-08 12:14:30 +0200

    added special case for 1 philo

--- philo/src/create_threads.c
+++ philo/src/create_threads.c
@@ -6,7 +6,7 @@
 /*   By: sschelti <sschelti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 14:23:28 by sschelti          #+#    #+#             */
-/*   Updated: 2023/06/06 17:20:32 by sschelti         ###   ########.fr       */
+/*   Updated: 2023/06/08 12:12:11 by sschelti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,41 @@ void	*philo_start(void *phi_struct)
 	philo = (t_philo *)phi_struct;
 	if (philo->i % 2 != 0)
 		accurate_usleep(philo->data->time_to_eat);
-	while (1)
-	{
-		pthread_mutex_lock(philo->data->finished_mutex);
-		if (philo->data->finished == true)
+	if (philo->data->num_of_philo > 1)
+	{	
+		while (1)
 		{
+			pthread_mutex_lock(philo->data->finished_mutex);
+			if (philo->data->finished == true)
+			{
+				pthread_mutex_unlock(philo->data->finished_mutex);
+				break;
+			}
 			pthread_mutex_unlock(philo->data->finished_mutex);
-			break;
-		}
-		pthread_mutex_unlock(philo->data->finished_mutex);
-		philo_eat(philo);
-		pthread_mutex_lock(philo->data->eat_mutex);
-		if (++philo->eat_n == philo->data->max_eat)
-		{
+			philo_eat(philo);
+			pthread_mutex_lock(philo->data->eat_mutex);
+			if (++philo->eat_n == philo->data->max_eat)
+			{
+				pthread_mutex_unlock(philo->data->eat_mutex);
+				break ;
+			}
 			pthread_mutex_unlock(philo->data->eat_mutex);
-			break ;
+			philo_sleep(philo);
+			print_update("is thinking", NULL, NULL, philo);
 		}
-		pthread_mutex_unlock(philo->data->eat_mutex);
-		philo_sleep(philo);
-		print_update("is thinking", NULL, NULL, philo);
+	}
+	else
+	{
+		while (1)
+		{
+			pthread_mutex_lock(philo->data->finished_mutex);
+			if (philo->data->finished == true)
+			{
+				pthread_mutex_unlock(philo->data->finished_mutex);
+				break;
+			}
+			pthread_mutex_unlock(philo->data->finished_mutex);
+		}
 	}
 	return (NULL);
 }
